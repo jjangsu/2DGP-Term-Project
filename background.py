4e2a82ff7e033f73c9fb387ac2a1619be6f51649
jjangsu <a01056386069@gmail.com>
2018-10-15 02:17:06 +0900

    class background
    배경 클래스의 뼈대를 만들었다.

---
+++ background.py
@@ -0,0 +1,16 @@
+from pico2d import *
+import random
+import os
+
+import game_framework
+
+class Background:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+    

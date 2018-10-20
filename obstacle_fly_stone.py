87b4b16bdeb44da7c555ccbe5bc15df55df3645c
jjangsu <a01056386069@gmail.com>
2018-10-20 21:05:11 +0900

    fly stone
    fly stone이 바닥에 하나만 생기는 클래스를 만들었다

--- obstacle_fly_stone.py
+++ obstacle_fly_stone.py
@@ -1,29 +1,28 @@
-from pico2d import *
+import random
 import obstacle_fly_stone_1
 
 class Fly_stone:
-    image = None
     def __init__(self, row, col, type):
-        if Fly_stone.image == None:
-            self.image = load_image(
-                'resource\Episode 1 - Escape from the Oven/1. The Witch Oven/epN01_tm01_bl1_ing_sprite.png')
-            self.x = ((106 * 12) * col + 106 * row - 0) * 2
-            if type == 0:
-                self.y = 500 / 2 - 140
-            elif type == 1:
-                self.y = 500 / 2 - 85
-            self.frame = 0
-            self.time = 0
-        pass
+        # if Fly_stone.image == None:
+        #     self.image = load_image(
+        #         'resource\Episode 1 - Escape from the Oven/1. The Witch Oven/epN01_tm01_bl1_ing_sprite.png')
+        #     self.x = ((106 * 12) * col + 106 * row - 0) * 2
+        #     if type == 0:
+        #         self.y = 500 / 2 - 140
+        #     elif type == 1:
+        #         self.y = 500 / 2 - 85
+        #     self.frame = 0
+        #     self.time = 0
+        self.stone_type = 0 #random.randint(0, 1)
 
+        if self.stone_type == 0:
+            self.fly_stone = obstacle_fly_stone_1.Stone_1(row, col)
+        pass
+#
     def update(self):
-        self.x -= 2.0
-        self.time += 1
-        if self.time > 20:
-            self.frame = (self.frame + 1) % 3
-            self.time = 0
+        self.fly_stone.update()
         pass
 
     def draw(self):
-        self.image.clip_draw(self.frame * 200, 0, 202, 117, self.x, self.y)
+        self.fly_stone.draw()
         pass
--- obstacle_fly_stone_1.py
+++ obstacle_fly_stone_1.py
@@ -1,11 +1,25 @@
 from pico2d import *
 
 class Stone_1:
-    def __init__(self):
+    image = None
+    def __init__(self, col, row):
+        if Stone_1.image == None:
+            self.image = load_image(
+                'resource\Episode 1 - Escape from the Oven/1. The Witch Oven/epN01_tm01_bl1_ing_sprite.png')
+            self.x = ((106 * 12) * col + 106 * row - 0) * 2
+            self.y = 500 / 2 - 140
+            self.frame = 0
+            self.time = 0
         pass
 
     def update(self):
+        self.x -= 2.0
+        self.time += 1
+        if self.time > 20:
+            self.frame = (self.frame + 1) % 3
+            self.time = 0
         pass
 
     def draw(self):
+        self.image.clip_draw(self.frame * 200, 0, 202, 117, self.x, self.y)
         pass

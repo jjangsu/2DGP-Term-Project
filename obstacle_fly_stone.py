f696f5cc88dd8fdf724fa9df70a95ed7b8725441
jjangsu <a01056386069@gmail.com>
2018-10-20 21:09:04 +0900

    fly stone
    fly stone4개가 날아오는 장애물을 만들었다

--- obstacle_fly_stone.py
+++ obstacle_fly_stone.py
@@ -1,22 +1,13 @@
 import random
 import obstacle_fly_stone_1
+import obstacle_fly_stone_4
 
 class Fly_stone:
     def __init__(self, row, col, type):
-        # if Fly_stone.image == None:
-        #     self.image = load_image(
-        #         'resource\Episode 1 - Escape from the Oven/1. The Witch Oven/epN01_tm01_bl1_ing_sprite.png')
-        #     self.x = ((106 * 12) * col + 106 * row - 0) * 2
-        #     if type == 0:
-        #         self.y = 500 / 2 - 140
-        #     elif type == 1:
-        #         self.y = 500 / 2 - 85
-        #     self.frame = 0
-        #     self.time = 0
-        self.stone_type = 0 #random.randint(0, 1)
-
-        if self.stone_type == 0:
+        if type == 0:
             self.fly_stone = obstacle_fly_stone_1.Stone_1(row, col)
+        elif type == 1:
+            self.fly_stone = obstacle_fly_stone_4.Stone_4(row, col)
         pass
 #
     def update(self):

ae8a3e633e2665f7cd88b50aa85027f7556ecafc
jjangsu <a01056386069@gmail.com>
2018-10-22 03:10:45 +0900

    mouse
    mouse커서의 모양을 바꿨다

--- scene_robby.py
+++ scene_robby.py
@@ -8,20 +8,28 @@ top_image_x_2 = 500 + 1030
 bottom_image = None
 bottom_image_x = 500
 bottom_image_x_2 = 500 + 1314
+mouse = None
+mouse_x = 0
+mouse_y = 0
 
 def enter():
-    global top_image, bottom_image, top_image_2
+    global top_image, bottom_image, top_image_2, mouse, mouse_x, mouse_y
     if top_image == None:
         top_image = load_image('resource/robby_top1.png')
 
     if bottom_image == None:
         bottom_image = load_image('resource/robby_bottom.png')
+
+    if mouse == None:
+        hide_cursor()
+        mouse = load_image('resource/mouse1.png')
     pass
 
 def exit():
-    global top_image, bottom_image
+    global top_image, bottom_image, mouse
     del (top_image)
     del (bottom_image)
+    del (mouse)
     pass
 
 
@@ -46,16 +54,21 @@ def update():
 def draw():
     global top_image, bottom_image, top_image_2, bottom_image_x_2
     clear_canvas()
-    top_image.clip_draw(0, 0, 1030, 246, top_image_x, 250 + 123)
-    top_image.clip_draw(0, 0, 1030, 246, top_image_x_2, 250 + 123)
-    bottom_image.clip_draw(0, 0, 1314, 394, bottom_image_x, 250 - 197)
-    bottom_image.clip_draw(0, 0, 1314, 394, bottom_image_x_2, 250 - 197)
+    top_image.clip_draw(0, 0, 1030, 246, top_image_x, 250 + 125)
+    top_image.clip_draw(0, 0, 1030, 246, top_image_x_2, 250 + 125)
+    bottom_image.clip_draw(0, 0, 1314, 394, bottom_image_x, 250 - 195)
+    bottom_image.clip_draw(0, 0, 1314, 394, bottom_image_x_2, 250 - 195)
+    mouse.clip_draw(0, 0, 80, 80, mouse_x, mouse_y)
     update_canvas()
     pass
 
 
 def handle_events():
+    global mouse_x, mouse_y
     events = get_events()
+    for event in events:
+        if event.type == SDL_MOUSEMOTION:
+            mouse_x, mouse_y = event.x, 500 - 1 - event.y
     pass
 
 

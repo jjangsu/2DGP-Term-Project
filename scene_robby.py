4c6cc70383b7b5ea993ca73db81c1f89007c7bf0
jjangsu <a01056386069@gmail.com>
2018-10-22 02:00:19 +0900

    robby class
    robby class의 뼈대를 만들었다

---
+++ scene_robby.py
@@ -0,0 +1,31 @@
+from pico2d import *
+import game_framework
+
+image = None
+start_time = 0.0
+
+def enter():
+    pass
+
+def exit():
+   pass
+
+
+def update():
+    pass
+
+
+def draw():
+    pass
+
+
+def handle_events():
+    pass
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass

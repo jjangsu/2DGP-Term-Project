01198dda76e29bb855a67ba6c55ddef4662343a7
jjangsu <a01056386069@gmail.com>
2018-10-25 00:57:21 +0900

    캐릭터 클래스
    용감한맛 쿠키와 명량한 쿠키가 상속받을 클래스를 만들 예정이다. 이 클래스를 이용하여 각각의 클래스를 만들어줄것이다

---
+++ character.py
@@ -0,0 +1,14 @@
+from pico2d import *
+
+class Brave():
+    def __init__(self):
+        pass
+
+    def newPosition(self, x, y):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass

b8402557795de471f133143a874256248cf6a10a
jjangsu <a01056386069@gmail.com>
2018-10-25 01:04:25 +0900

    캐릭터 클래스
    모든 캐릭터의 기본이 될 클래스이다

--- character.py
+++ character.py
@@ -1,14 +1,28 @@
 from pico2d import *
 
-class Brave():
+class Character:
     def __init__(self):
+        self.x = 200
+        self.y = 70 + 115
+        self.image_y = 4
+        self.frame = 0
+        self.time = 0
+        self.standard_time = 3.0
+        self.image = None
         pass
 
     def newPosition(self, x, y):
+        self.x = x
+        self.y = y
         pass
 
     def update(self):
+        self.time += 1
+        if self.time > self.standard_time:
+            self.frame = (self.frame + 1) % 4
+            self.time = 0
         pass
 
     def draw(self):
+        self.image.clip_draw(self.frame * 236, self.image_y * 236, 236, 236, self.x, self.y)
         pass

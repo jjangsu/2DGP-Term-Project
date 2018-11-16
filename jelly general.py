1fb3874de1c932be9972e971ab8c51a9fbe571aa
jjangsu <a01056386069@gmail.com>
2018-11-16 20:27:44 +0900

    genaral jelly
    genaral jelly class make

---
+++ jelly general.py
@@ -0,0 +1,13 @@
+import game_framework
+import jelly
+
+
+class General(jelly.Jelly):
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def get_bb(self):
+        pass
--- jelly.py
+++ jelly.py
@@ -2,6 +2,12 @@ import game_framework
 
 class Jelly:
     def __init__(self):
+        self.x = 100
+        self.y = 70
+        self.speed = 200
+        self.image = None
+
+        self.frame = 0
         pass
 
 

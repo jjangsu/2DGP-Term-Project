25c003c0a23f32440847e67d73aa2d549fe2afe6
jjangsu <a01056386069@gmail.com>
2018-11-19 00:26:21 +0900

    add sliver coin class

---
+++ jelly_silver_coin.py
@@ -0,0 +1,22 @@
+from pico2d import *
+import game_framework
+import jelly
+
+
+TIME_PER_ACTION = 0.4
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 6
+
+
+class SilverCoin(jelly.Jelly):
+    def nitialize(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def get_bb(self):
+        pass

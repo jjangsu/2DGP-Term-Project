b0437396e0c2daa39fd32df4d0afeda486b176e7
jjangsu <a01056386069@gmail.com>
2018-11-19 00:50:48 +0900

    make pink bear jelly class

---
+++ jelly_pink_bear.py
@@ -0,0 +1,14 @@
+from pico2d import *
+import game_framework
+import jelly
+
+
+class PinkBear(jelly.Jelly):
+    def initialize(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def get_bb(self):
+        pass

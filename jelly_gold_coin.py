3f389b8f31245e5577581dbf7a9782239c95506a
jjangsu <a01056386069@gmail.com>
2018-11-19 00:00:01 +0900

    make gold coin jelly class

---
+++ jelly_gold_coin.py
@@ -0,0 +1,15 @@
+from pico2d import *
+import jelly
+
+
+TIME_PER_ACTION = 0.4
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 2
+
+
+class GoldCoin(jelly.Jelly):
+    def initialize(self):
+        pass
+
+    def draw(self):
+        pass
